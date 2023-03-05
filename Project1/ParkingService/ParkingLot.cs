838701b61e75b28c864348d0b9a37a2de336e509
shshshshshan <21-1-01040@vsu.edu.ph>
2023-03-05 10:25:07 +0800

    Moved all files to new folder

---
+++ Project1/ParkingService/ParkingLot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingService
+{
+    public class ParkingLot
+    {
+        public string BranchID { get; private set; }
+        public string BranchName { get; private set; }
+        public string Location { get; set; }
+        public int MaxCapacity { get; private set; }
+        public decimal RatePerHour { get; set; }
+        public List<ParkingSpace> Spaces;
+
+        public ParkingLot(string branchid, string branchname, string location, int maxcap, decimal rph) 
+        {
+            this.BranchID = branchid;
+            this.BranchName = branchname;
+            this.Location = location;
+            this.MaxCapacity = maxcap;
+            this.RatePerHour = rph;
+            this.Spaces = new List<ParkingSpace>(maxcap);
+        }
+        public ParkingLot(string branchid, string branchname, string location, decimal rph, List<ParkingSpace> sp)
+        {
+            this.BranchID = branchid;
+            this.BranchName = branchname;
+            this.Location = location;
+            this.MaxCapacity = sp.Count;
+            this.RatePerHour = rph;
+            this.Spaces = sp;
+        }
+
+        public void SetMaxCapacity(int newCapacity)
+        {
+            this.MaxCapacity = newCapacity;
+        }
+    }
+}
---
+++ Project1/ParkingService/ParkingSpace.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingService
+{
+    public class ParkingSpace
+    {
+        public string SpaceID { get; private set; }
+        public bool Availability { get; private set; }
+        public decimal PricePerHour { get; private set; }
+        public double Area { get; private set; }
+        public Accesibility Accessibility { get; private set; }
+        public ParkingLot Lot;
+
+        public ParkingSpace(string spaceid, bool availability, decimal pph, ParkingLot lot)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = availability;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = 0.0;
+            this.Accessibility = Accesibility.None;
+        }
+        public ParkingSpace(string spaceid, bool availability, decimal pph, ParkingLot lot, double a)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = availability;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = a;
+            this.Accessibility = Accesibility.None;
+        }
+        public ParkingSpace(string spaceid, bool availability, decimal pph, ParkingLot lot, double a, Accesibility accessibility)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = availability;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = a;
+            this.Accessibility = accessibility;
+        }
+
+        public bool IsOccupied()
+        {
+            // If this.Availablitiy -> not occupied since available
+            // Else -> occupied since not available
+
+            return !this.Availability;
+        }
+    }
+}
