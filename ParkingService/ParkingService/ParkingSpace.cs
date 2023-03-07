9ff0f9a62eec56fb8247e290fecab93f7a24fc9c
shshshshshan <21-1-01040@vsu.edu.ph>
2023-03-07 13:38:30 +0800

    Added new files

---
+++ ParkingService/ParkingService/ParkingSpace.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingService
+{
+    public class ParkingSpace
+    {
+        public string SpaceID { get; private set; }
+        public bool Availability { get; private set; }
+        public decimal PricePerHour { get; private set; } // In Pesos
+        public double Area { get; private set; } // The area in meters 
+        public Accesibility Accessibility { get; private set; }
+        public ParkingLot Lot;
+
+        public ParkingSpace(string spaceid, decimal pph, ParkingLot lot)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = true;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = 0.0;
+            this.Accessibility = Accesibility.None;
+        }
+
+        public ParkingSpace(string spaceid, bool availability, decimal pph, ParkingLot lot)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = availability;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = 0.0;
+            this.Accessibility = Accesibility.None;
+        }
+        public ParkingSpace(string spaceid, bool availability, decimal pph, ParkingLot lot, double a)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = availability;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = a;
+            this.Accessibility = Accesibility.None;
+        }
+        public ParkingSpace(string spaceid, bool availability, decimal pph, ParkingLot lot, double a, Accesibility accessibility)
+        {
+            this.SpaceID = spaceid;
+            this.Availability = availability;
+            this.PricePerHour = pph;
+            this.Lot = lot;
+            this.Area = a;
+            this.Accessibility = accessibility;
+        }
+
+        public bool IsOccupied()
+        {
+            // If this.Availablitiy -> not occupied since available
+            // Else -> occupied since not available
+
+            return !this.Availability;
+        }
+
+        public decimal TotalCost(DateTime inTime, DateTime outTime)
+        {
+            // The formula for getting a cost for staying in a certain parking space
+            // The formula is subjective:
+            // total cost = (out - in) * (area of parking space / 100) + (price per hour * accessibility)
+            return ((decimal)(outTime - inTime).TotalMinutes) * (decimal)(this.Area / this.Lot.GetTotalParkingSpaces()) + (this.PricePerHour * ((int)this.Accessibility));
+        }
+    }
+}
